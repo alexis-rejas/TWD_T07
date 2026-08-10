e0a10f7f05e97d65210df3404cb905ee170a73bb
alexis-rejas <oscar.rejas@vallegrande.edu.pe>
2026-08-10 15:11:20 -0500

    redireccion en el app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, send_from_directory
 import os
 
-app = Flask(__name__, template_folder='templates', static_folder='.', static_url_path='')
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/node_modules/<path:filename>')
+def serve_node_modules(filename):
+    return send_from_directory(os.path.join(os.getcwd(), 'node_modules'), filename)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
